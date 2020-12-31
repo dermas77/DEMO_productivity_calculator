7274e9916ec945b1b8b91c127bb0a4d1d363efcd
dermas77 <dermas77@gmail.com>
2020-12-31 22:00:55 +0200

    datesview

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
         <div class="row justify-content-center">
             <div class="col-md-6">
                 <div class="card">
-                    <div class="card-header">{{ __('Shifts Chosen') }}</div>
+                    <div class="card-header">{{ __('Shifts Chosen  ') }} @if (isset($_GET['check-start']) == 1){{$_GET['check-start']}} @else No start date @endif - @if (isset($_GET['check-end']) == 1){{$_GET['check-end']}} @else No end date @endif</div>
                     <div class="card-body">
                         @foreach ($shifts as $shift)
                             <li style="list-style: none">Date: <i>{{ $shift->date }}</i> </li>
@@ -25,17 +25,16 @@
 
                         <label for="start">Start date:</label>
                         <input type="date" id="start" name="check-start" min="2020-01-01" max="2021-12-31"
-                            placeholder="dd-mm-yyyy" value="2020-01-01">
+                            placeholder="dd-mm-yyyy" value="">
                         <label for="start">End date:</label>
                         <input type="date" id="start" name="check-end" min="2020-01-01" max="2021-12-31"
-                            placeholder="dd-mm-yyyy" value="2020-01-01">
+                            placeholder="dd-mm-yyyy" value="">
                         <button type="submit" class="btn btn-primary">Submit</button>
                     </form>
                 </div>
+
             </div>
-
             @include('data')
-
         </div>
     </div>
 @endsection
